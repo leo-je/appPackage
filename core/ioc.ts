2fd4e032ec1e435ee40a9c4009275ca71dce4917
te <shengteli@caih.com>
2022-11-16 19:02:15 +0800

    fix

--- core/ioc.ts
+++ core/ioc.ts
@@ -3,6 +3,8 @@ import path from 'path'
 import { Controllers } from './decorator/reflect-metadata/decorator';
 import register from './decorator/reflect-metadata/register';
 
+const requireMap = {}
+
 export const enableIoc = (app, rootPath: string, scanDirPaths: string[]) => {
     for (let i in scanDirPaths) {
         readDir((rootPath + "/" + scanDirPaths[i]).replace('//', '/'), rootPath);
@@ -47,4 +49,6 @@ function requireComponent(filePath: string, className: string) {
     // console.log(filePath)
     console.log(`requireComponent`, className)
     let _t = require(filePath)
+    console.log(`requireComponent-add`, className)
+    requireMap[className.replace('.js','').replace('.ts','')] = _t
 }
